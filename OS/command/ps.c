0d6d1cfa957d3d6dbbeecb8b7b7b484c80f59ee5
HOLLYwyh <1689394668@qq.com>
2021-08-12 22:21:14 +0800

    添加ps框架

--- OS/command/ps.c
+++ OS/command/ps.c
@@ -1,8 +1,26 @@
 # include "stdio.h"
+# include "shell.h"
 
 //进程相关
 int main(int argc, char* argv[])
 {
+	int num = argc;
+	char ** array = argv;//使用num 和argv两个变量来接收argc和argv
 
+	if(num >=2)
+	{
+		//help: -h or --help
+		if((strequal(array[1],help1))||(strequal(array[1],help2)))
+		{
+		}
+		//all: --all
+		else if(strequal(array[1],all))
+		{
+		}
+	}
+	//ps
+	else
+	{
+	}
 	return 0;
 }
